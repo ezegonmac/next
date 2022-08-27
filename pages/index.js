380e22bc5b2f921fcc047d0273a57a3de6f0f982
unknown <ezegonmac01@gmail.com>
2022-08-27 15:31:29 +0100

    Add Image component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import Image from "next/image"
 import Link from "next/link"
+import Coffee from '../public/coffee.JPG'
 
 export default function Home() {
   return (
     <div>
       <p>Chanchito feliz</p>
+      {/* <Image src='/coffee.JPG' width={400} height={400}/> */}
+      {/* <Image src='/coffee.JPG' layout="fill"/> */}
+      <Image src={Coffee} width={400} height={400}/>
       <Link href="/chanchitos">Ir a Chanchitos</Link>
   </div>
   )
